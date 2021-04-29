16f966e8dd0bcb1e8b4c8136c7550faf44ca5943
David Filimon <davidfilimon2000@yahoo.com>
2021-04-29 11:10:53 +0300

    PYS-4 #made create account button

---
+++ src/main/java/PickYourSpot/Main.java
@@ -0,0 +1,55 @@
+package PickYourSpot;
+
+import PickYourSpot.services.UserService;
+import PickYourSpot.services.FileSystemService;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+public class Main extends Application {
+
+    private static Scene scene;
+    private static Stage window;
+
+    public static Stage getWindow() {
+        return window;
+    }
+    public static Scene getScene() {
+        return scene;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        initDirectory();
+        UserService.initDatabase();
+        window = primaryStage;
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("firstpage.fxml")));
+        primaryStage.setTitle("Pick Your Spot");
+        scene = new Scene(root, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    private void initDirectory() {
+        Path applicationHomePath = FileSystemService.APPLICATION_HOME_PATH;
+        if (!Files.exists(applicationHomePath))
+            applicationHomePath.toFile().mkdirs();
+    }
+
+
+}
