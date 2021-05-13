f9327e7b759fab376357d528b02d32a0f70fbc80
ManeaCiprian <Chemirol410$>
2021-05-13 18:52:42 +0300

    PYS-11 #comment added restriction for deleting a movie

--- src/main/java/PickYourSpot/Controllers/SeeMovieListController.java
+++ src/main/java/PickYourSpot/Controllers/SeeMovieListController.java
@@ -12,10 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -157,10 +154,18 @@ public class SeeMovieListController {
     }
 
     public void deleteButtonClicked(){
-        MovieService.findAndRemove(movie);
-        MovieService.emptyMovieData();
-        sw = true;
-        initialize();
+        if(!ReservationService.isEmpty()){
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setContentText("There are still reservations available. You\n cannot delete a movie yet!");
+            a.show();
+        }
+            else{
+            MovieService.findAndRemove(movie);
+            MovieService.emptyMovieData();
+            sw = true;
+            initialize();
+        }
+
     }
 
     public void editButtonClicked() throws IOException {
